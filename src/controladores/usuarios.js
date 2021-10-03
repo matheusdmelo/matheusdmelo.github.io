5543821aa38e261c120a28ccc0529c99e2f02d72
Matheus Melo <m.melo2000@gmail.com>
2021-10-03 06:17:40 -0300

    Endpoint Cadastro e Login

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,7 +1,43 @@
 const conexao = require('../conexao');
-const bcyypt = require('bcypt');
+const bcrypt = require('bcrypt');
 
 const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    if (!nome) {
+        return res.status(404).json("O campo nome é obrigatorio");
+    }
+
+    if (!email) {
+        return res.status(404).json("O campo email é obrigatorio");
+    }
+
+    if (!senha) {
+        return res.status(404).json("O campo senha é obrigatorio");
+    }
+
+    try {
+        const { rowCount: quantidadeUsuarios } = await conexao.query('select * from usuarios where email = $1', [email]);
+
+        if (quantidadeUsuarios > 0) {
+            return res.status(400).json("O email já foi cadastrado");
+        }
+        
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const query = 'insert into usuarios (nome, email, senha) values ($1, $2, $3)';
+
+        const usuario = await conexao.query(query, [nome, email, senhaCriptografada]);
+
+        if (usuario.rowCount === 0) {
+            return res.status(400).json("O usuario não foi cadastrado");
+        }
+
+        return res.status(200).json("O usuario foi cadastrado com sucesso");
+        
+    } catch (error) { 
+        return res.status(400).json(error.message)
+    }
 
 }
 
--- src/rotas.js
+++ src/rotas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
 const clientes = require('./controladores/clientes');
-//const verificaLogin = require('./controladores/clientes');
+// const verificaLogin = require('./filtros/verificalogin');
 
 const rotas = express();
 
@@ -13,17 +13,17 @@ rotas.post('/usuarios', usuarios.cadastrarUsuario),
 rotas.post('/login', login.login);
 
 // filtro para verificar usuario logado
-rotas.use(verificaLogin);
+// rotas.use(verificaLogin);
 
 // obter e atualizar perfil do usuario
 rotas.get('/perfil', usuarios.obterPerfil);
 rotas.put('/perfil', usuarios.atualizarPerfil);
 
 // crud de clientes
-rotas.get('/clientes', clientes.listarClientes.);
-rotas.get('/clientes/:id', clientes.obterCliente.);
-rotas.post('/clientes', clientes.cadastrarCliente.);
-rotas.put('/clientes/:id', clientes.atualizarCliente.)
-rotas.delete('/clientes/:id', clientes.excluirCliente.);
+rotas.get('/clientes', clientes.listarClientes);
+rotas.get('/clientes/:id', clientes.obterCliente);
+rotas.post('/clientes', clientes.cadastrarCliente);
+rotas.put('/clientes/:id', clientes.atuaizarCliente);
+rotas.delete('/clientes/:id', clientes.excluirCliente);
 
 module.exports = rotas;
