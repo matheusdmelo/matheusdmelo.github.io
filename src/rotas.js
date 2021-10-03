fddac7bca3db78709de8c14e3c7f95aa24337483
Matheus Melo <m.melo2000@gmail.com>
2021-10-03 02:25:45 -0300

    Rotas

--- src/rotas.js
+++ src/rotas.js
@@ -2,8 +2,28 @@ const express = require('express');
 const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
 const clientes = require('./controladores/clientes');
+//const verificaLogin = require('./controladores/clientes');
 
 const rotas = express();
 
+//cadastro do usuario
+rotas.post('/usuarios', usuarios.cadastrarUsuario),
+
+// login 
+rotas.post('/login', login.login);
+
+// filtro para verificar usuario logado
+rotas.use(verificaLogin);
+
+// obter e atualizar perfil do usuario
+rotas.get('/perfil', usuarios.obterPerfil);
+rotas.put('/perfil', usuarios.atualizarPerfil);
+
+//crud de clientes
+rotas.get('/clientes', clientes.listarClientes.);
+rotas.get('/clientes/:id', clientes.obterCliente.);
+rotas.post('/clientes', clientes.cadastrarCliente.);
+rotas.put('/clientes/:id', clientes.atualizarCliente.)
+rotas.delete('/clientes/:id', clientes.excluirCliente.);
 
 module.exports = rotas;
