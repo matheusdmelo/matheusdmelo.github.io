72e5f84394c8746b6f49d62ab81fd8557b826610
Matheus Melo <m.melo2000@gmail.com>
2021-10-03 10:11:08 -0300

    banco de dados na heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg'); 
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'desafio_5',
-    password: '123456',
-    port: 5432 
+    user: 'cdhlapgvqqzjox',
+    host: 'ec2-34-232-191-133.compute-1.amazonaws.com',
+    database: 'dbqdtlsmhuq1fu',
+    password: '3bba1c6ed0e5b65f493d795330a9f2b1ec47ba239f0f068f1b601a2e11a25e2e',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
