2982f0e7cad9d07a78992311f7bd3c58b0da04b9
Matheus Melo <m.melo2000@gmail.com>
2021-10-04 16:55:35 -0300

    Ponto de referencia

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -36,7 +36,7 @@ const obterCliente = async (req, res) => {
 
 const cadastrarCliente = async (req, res) => {
     const { usuario } = req;
-    const {nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado} = req.body;
+    const {nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado, ponto_de_referencia} = req.body;
 
     if (!nome) {
         return res.status(404).json('O campo nome é obrigatorio');
@@ -55,8 +55,8 @@ const cadastrarCliente = async (req, res) => {
     }
 
     try {
-        const query = 'insert into clientes (usuario_id, nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)';        
-        const cliente = await conexao.query(query, [usuario.id, nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado]);
+        const query = 'insert into clientes (usuario_id, nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado, ponto_de_referencia) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';        
+        const cliente = await conexao.query(query, [usuario.id, nome, email, cpf, telefone, cep, logradouro, complemento, bairro, cidade, estado, ponto_de_referencia]);
         
         if (cliente.rowCount === 0) {
             return res.status(400).json('O cliente não foi cadastrado');
