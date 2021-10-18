95d95248bc319a1f2db1d41f5f120a6b6f958fcd
Matheus Melo <m.melo2000@gmail.com>
2021-10-18 17:20:54 -0300

    ndvundui

--- src/controladores/cobrancas.js
+++ src/controladores/cobrancas.js
@@ -55,12 +55,12 @@ const cadastrarCobranca = async (req, res) => {
 }
 
 const excluirCobranca = async (req, res) => {
-    const { cliente } = req;
+    const { usuario, cliente } = req;
     const { id } = req.params
 
     try {
-        const query = 'select * from cobrancas where cliente_id = $1 and id = $2';
-        const { rowCount } = await conexao.query(query, [cliente.id, id]);
+        const query = 'select * from cobrancas where usuario_id = $1, cliente_id = $2 and id = $3';
+        const { rowCount } = await conexao.query(query, [usuario.id, cliente.id, id]);
 
         if (rowCount === 0) {
             return res.status(404).json('Cobrança não encontrada');
